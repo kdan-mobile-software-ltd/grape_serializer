fb1149cc614314633c86e8c3750c5b830c3d5368
JiaRou <laura34963@kdanmobile.com>
2022-11-04 09:51:51 +0800

    Add append info to service_entity

--- lib/service_entity.rb
+++ lib/service_entity.rb
@@ -2,6 +2,9 @@ require 'grape-entity'
 
 class ServiceEntity < Grape::Entity
 
+  # expose append info
+  expose :append_info, merge: true, if: :append
+
   # expose service additional info by default
   expose :service_info, merge: true, if: :service
 
@@ -12,4 +15,9 @@ class ServiceEntity < Grape::Entity
     service_name = service.class.name.underscore
     send(service_name) rescue nil || send(service_name.split('/').last) rescue {}
   end
+
+  def append_info
+    options[:append]
+  end
+  
 end
