3909d00853efd3e32b77d446c4992f410eac121b
JiaRou <laura34963@kdanmobile.com>
2022-11-14 14:44:18 +0800

    Update ListEntity

--- lib/list_entity.rb
+++ lib/list_entity.rb
@@ -4,10 +4,8 @@ class ListEntity < ServiceEntity
     subclass.present_collection true
 
     subclass.expose :total_count
-    if pagination_items.respond_to?(:current_page)
-      subclass.expose :current_page
-      subclass.expose :total_pages
-    end
+    subclass.expose :current_page
+    subclass.expose :total_pages
   end
 
   private
@@ -19,17 +17,25 @@ class ListEntity < ServiceEntity
   def total_count
     if pagination_items.respond_to?(:total_count)
       pagination_items.total_count
-    elsif pagination_items.is_a?(Array)
+    elsif pagination_items.respond_to?(:length)
       pagination_items.length
     end
   end
 
   def current_page
-    pagination_items.current_page
+    if pagination_items.respond_to?(:current_page)
+      pagination_items.current_page
+    else
+      1
+    end
   end
   
   def total_pages
-    pagination_items.total_pages
+    if pagination_items.respond_to?(:total_pages)
+      pagination_items.total_pages
+    else
+      0
+    end
   end
   
 end
